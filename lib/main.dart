5853b33e186d3d3872061e688d613e505aba9fcb
Jhon Giraldo <jhon.giraldo@pragma.com.co>
2023-01-18 12:22:24 -0500

    Counter App using Streams

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'counter_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Material App', home: CounterScreen());
+  }
+}
